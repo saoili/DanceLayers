581f0ec2a58e610562d509318daa610a9ceceb5b
saoili <saoili@gmail.com>
2016-09-01 18:51:12 +0100

    Made compatible with python3

--- dance_layers.py
+++ dance_layers.py
@@ -5,16 +5,24 @@ from random import choice
 START_WITH_FEET = True
 
 
+def part_and_move(dance_parts, part=None):
+    if not part:
+        part = choice(list(dance_parts.keys()))
+    move = choice(dance_parts.pop(part))
+    return part, move
+
+
 if __name__ == "__main__":
     with open(sys.argv[1], 'r') as inputs:
         file_contents = inputs.read()
-        as_dict = json.loads(file_contents)
-        first_part = "feet" if START_WITH_FEET else choice(as_dict.keys())
-        first = choice(as_dict.pop(first_part))
-        second_part = choice(as_dict.keys())
-        second = choice(as_dict.pop(second_part))
-        third_part = choice(as_dict.keys())
-        third = choice(as_dict.pop(third_part))
-        print "Start with {}: {},\nAdd {}: {},\nThen add {}: {}".format(
-            first_part, first, second_part, second, third_part, third)
-
+        dance_parts = json.loads(file_contents)
+        first_part = "feet" if START_WITH_FEET else None
+        first_part, first_move = part_and_move(dance_parts, first_part)
+        second_part, second_move = part_and_move(dance_parts)
+        third_part, third_move = part_and_move(dance_parts)
+        print (
+            "Start with {}: {},\nAdd {}: {},\nThen add {}: {}".format(
+                first_part, first_move, second_part, second_move,
+                third_part, third_move
+            )
+        )
